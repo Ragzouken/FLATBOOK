607788c81ed8b2b5c2bd787ca092fbd0b4fef530
mark <ragzouken@gmail.com>
2017-06-10 15:55:22 +0100

    rotate animation

---
+++ Assets/Main Scripts/AnimateRotate.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using Random = UnityEngine.Random;
+
+[ExecuteInEditMode]
+public class AnimateRotate : MonoBehaviour 
+{
+    [Header("Timings (seconds)")]
+    public float turnSpeed;
+    public float turnDuration;
+
+    #region Editor Tricks
+    private float _prevTurnSpeed;
+    private float _prevTurnDuration;
+
+    /// <summary>
+    /// update timings so that loop duration, frame duration, and frame count
+    /// are consistent with each other, based on which have just changed
+    /// </summary>
+    private void MakeTimingsConsistent()
+    {
+        if (_prevTurnSpeed != turnSpeed)
+        {
+            turnDuration = turnSpeed / 360f;
+        }
+        else if (_prevTurnDuration != turnDuration)
+        {
+            turnSpeed = 360f / turnDuration;
+        }
+        
+        _prevTurnSpeed = turnSpeed;
+        _prevTurnDuration = turnDuration;
+    }
+    #endregion
+
+    private void Update()
+    {
+        MakeTimingsConsistent();
+
+        if (Application.isPlaying)
+        {
+            transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime);
+        }
+    }
+}
